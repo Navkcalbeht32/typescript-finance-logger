d5c33be0f5842b205abec9c4f71a2bfe8b1a79f9
Navkcalbeht32 <asumoski@gmail.com>
2021-02-16 09:44:20 -0500

    first commit

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var person = 'alex';
+console.log(person);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const person = 'alex';
+
+console.log(person);
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+    console.log(input)
+})
